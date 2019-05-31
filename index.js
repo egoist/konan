ae6d6660c32330c78d150dd57b85defd9ba3270b
F1LT3R <al@pwn.io>
2019-05-31 12:08:46 -0400

    Add ability to get imports.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function(
   src,
   { dynamicImport = true, parse = { sourceType: 'module', plugins: '*' } } = {}
 ) {
-  const modules = { strings: [], expressions: [] }
+  const modules = { imports: {}, strings: [], expressions: [] }
 
   let ast
 
@@ -21,31 +21,83 @@ module.exports = function(
     ast = src
   }
 
+  const storeNamedImports = node => {
+
+    // console.log(node);
+
+    if (node.specifiers) {
+      // console.log('................');
+      // console.log(path.node)
+      const imported = node.specifiers.map(specifier => specifier.imported || specifier.local);
+      if (imported) {
+        // console.log(imported);
+
+        const imports = imported.map(indentifier => indentifier && indentifier.name);
+        // console.log(imports);
+
+        if (!Reflect.has(modules.imports, node.source.value)) {
+          modules.imports[node.source.value] = [];
+        }
+
+        modules.imports[node.source.value] = imports;
+      }
+    }
+  }
+
+  let prevNode;
+  let prevPath;
+
   traverse(ast, {
     enter(path) {
+      // console.log(path.node);
+      // console.log(path.node.type);
+
+
       if (path.node.type === 'CallExpression') {
         const callee = path.get('callee')
         const isDynamicImport = dynamicImport && callee.isImport()
         if (callee.isIdentifier({ name: 'require' }) || isDynamicImport) {
           const arg = path.node.arguments[0]
           if (arg.type === 'StringLiteral') {
-            modules.strings.push(arg.value)
+            modules.strings.push(arg.value);
+
+            // console.log(prevPath.parentPath);
+            if (Reflect.has(prevPath.parentPath, 'parentPath') && prevPath.parentPath.parentPath instanceof Object) {
+              const variableDeclarator = prevPath.parentPath.parentPath.parent;
+              const namedProps = variableDeclarator.id.properties.map(property => property.key.name);
+              if (!Reflect.has(modules.imports, arg.value)) {
+                modules.imports[arg.value] = [];
+              }
+              modules.imports[arg.value].push(...namedProps);
+              return
+            }
+
+            if (Reflect.has(path.node, 'arguments') && path.node.arguments.length > 0 && path.node.arguments[0].type === 'StringLiteral') {
+              modules.imports[arg.value] = ['default'];
+            }
+
           } else {
             modules.expressions.push(src.slice(arg.start, arg.end))
           }
+          // storeNamedImports(path.node);
         }
       } else if (
         path.node.type === 'ImportDeclaration' ||
         path.node.type === 'ExportNamedDeclaration' ||
         path.node.type === 'ExportAllDeclaration'
-      ) {
-        const { source } = path.node
-        if (source && source.value) {
-          modules.strings.push(source.value)
+        ) {
+          // console.log(path.node);
+
+          const { source } = path.node
+          if (source && source.value) {
+            modules.strings.push(source.value)
+            storeNamedImports(path.node);
+          }
         }
+
+        prevPath = path
       }
-    }
-  })
+    })
 
   return modules
 }
--- test/index.test.js
+++ test/index.test.js
@@ -3,19 +3,28 @@ const konan = require('../')
 
 describe('main', () => {
   const input = fs.readFileSync('./test/fixture.js', 'utf8')
+  const input2 = fs.readFileSync('./test/fixture2.js', 'utf8')
 
   test('all', () => {
-    expect(konan(input)).toEqual({
-      strings: ['foo', 'vue/dist/vue', 'wow', 'baby', './async-module'],
-      expressions: []
-    })
+    expect(konan(input).strings).toEqual(
+      ['foo', 'vue/dist/vue', 'wow', 'baby', './async-module']
+    )
+  })
+
+  test('all named imports', () => {
+    expect(konan(input2).imports).toEqual({
+      baz: ['foo', 'bar', 'qux'],
+      'vue/dist/vue': ['hahah'],
+      wow: ['hello'],
+      baby: ['hello2', 'hello3'],
+      './async-module': ['default']
+    });
   })
 
   test('exclude dynamical import', () => {
-    expect(konan(input, { dynamicImport: false })).toEqual({
-      strings: ['foo', 'vue/dist/vue', 'wow', 'baby'],
-      expressions: []
-    })
+    expect(konan(input, { dynamicImport: false }).strings).toEqual(
+      ['foo', 'vue/dist/vue', 'wow', 'baby'],
+    )
   })
 
   test('dynamical require', () => {
@@ -26,7 +35,8 @@ describe('main', () => {
     `)
     ).toEqual({
       strings: ['bar'],
-      expressions: ["path.resolve('./')"]
+      expressions: ["path.resolve('./')"],
+      imports: {bar: ['default']}
     })
   })
 
@@ -40,14 +50,16 @@ describe('main', () => {
     `)
     ).toEqual({
       strings: ['foo'],
-      expressions: []
+      expressions: [],
+      imports: {foo: ['default']}
     })
   })
 
   test('import *', () => {
     expect(konan(`import * as m from 'm';var foo = {import: 'mm'}`)).toEqual({
       strings: ['m'],
-      expressions: []
+      expressions: [],
+      imports: {m: ['m']}
     })
   })
 
@@ -55,7 +67,20 @@ describe('main', () => {
     const input = fs.readFileSync('./test/fixture-export.js', 'utf8')
     expect(konan(input)).toEqual({
       strings: ['./util', './temporary', './persistent', 'all'],
-      expressions: []
+      expressions: [],
+      imports: {
+        './persistent': [
+          "MDCPersistentDrawer",
+          "MDCPersistentDrawerFoundation"
+        ],
+        './temporary': [
+          "MDCTemporaryDrawer",
+          "MDCTemporaryDrawerFoundation"
+        ],
+        './util': [
+          'util'
+        ]
+      }
     })
   })
 })
